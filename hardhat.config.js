3db5b2eb54b1a49f003dc4cf390a3208b1b9ade8
Micah Alcorn <273937+micahalcorn@users.noreply.github.com>
2023-01-12 18:06:07 -0800

    Handle environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,12 +3,18 @@ require('@openzeppelin/hardhat-upgrades');
 
 require("./tasks/faucet");
 
+require('dotenv').config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
     }
   }
 };
